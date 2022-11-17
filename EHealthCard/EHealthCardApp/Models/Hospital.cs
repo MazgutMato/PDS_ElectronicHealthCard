9a01025c6b9b1358c5cb2f6aca2958c1ad7519b3
Maťo <matej.mazgut1@gmail.com>
2022-11-17 16:45:30 +0100

    Hospital controller

--- EHealthCard/EHealthCardApp/Models/Hospital.cs
+++ EHealthCard/EHealthCardApp/Models/Hospital.cs
@@ -1,14 +1,23 @@
-﻿using System;
+﻿using System.ComponentModel.DataAnnotations;
+using System;
 using System.Collections.Generic;
 
 namespace EHealthCardApp.Models;
 
 public partial class Hospital
 {
+    [Required]
+    [Key]
+    [StringLength(20,
+        ErrorMessage = "Hospital name has to be maximal 20 chars long")]
     public string HospitalName { get; set; } = null!;
-
+    [Required]
+    [StringLength(5,
+        ErrorMessage = "Zip code has to be 5 chars long",
+        MinimumLength = 5)]
     public string Zip { get; set; } = null!;
-
+    [Required]
+    [Range(1,10000)]
     public int Capacity { get; set; }
 
     public virtual ICollection<Hospitalization> Hospitalizations { get; } = new List<Hospitalization>();
