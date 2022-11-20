a9572cde07a0fcdf534b1e82e30196c59adeb96b
Matej Mažgút <mazgut13@stud.uniza.sk>
2022-11-20 14:47:03 +0100

    Payment

--- EHealthCard/EHealthCardApp/Models/InsuranceComp.cs
+++ EHealthCard/EHealthCardApp/Models/InsuranceComp.cs
@@ -1,17 +1,19 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
 
 namespace EHealthCardApp.Models
 {
     public partial class InsuranceComp
     {
-        public InsuranceComp()
-        {
-            Insurances = new HashSet<Insurance>();
-            Payments = new HashSet<Payment>();
-        }
-
+        [Key]
+        [StringLength(3,
+             ErrorMessage = "Company id has to be 3 chars long",
+             MinimumLength = 3)]
         public string CompId { get; set; } = null!;
+
+        [Required]
+        [StringLength(30)]
         public string CompName { get; set; } = null!;
 
         public virtual ICollection<Insurance> Insurances { get; set; }
--- EHealthCard/EHealthCardApp/Models/Payment.cs
+++ EHealthCard/EHealthCardApp/Models/Payment.cs
@@ -1,16 +1,57 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.ComponentModel.DataAnnotations;
 
 namespace EHealthCardApp.Models
 {
     public partial class Payment
     {
-        public decimal PaymentId { get; set; }
+        [Required]
+        public int PaymentId { get; set; }
+
+        [Required]
+        [StringLength(20,
+             ErrorMessage = "Hospital name has to be maximal 20 chars long")]
         public string HospitalName { get; set; } = null!;
+
+        [Required]
+        [StringLength(3,
+             ErrorMessage = "Company id has to be 3 chars long",
+             MinimumLength = 3)]
         public string CompId { get; set; } = null!;
+
+        [Required]
+        [DisplayFormat(ApplyFormatInEditMode = true, DataFormatString = "{0:dd/MM/yyyy}")]
         public DateTime PaymentDate { get; set; }
+
+        [Required]
+        [DisplayFormat(ApplyFormatInEditMode = true, DataFormatString = "{0:dd/MM/yyyy}")]
         public DateTime PaymentPeriod { get; set; }
+
+        [Required]
         public string Details { get; set; } = null!;
+
+        [Required]
+        [NotMapped]
+        public string CompBank { get; set; } = null!;
+
+        [Required]
+        [NotMapped]
+        public string HospitalBank { get; set; } = null!;
+
+        [Required]
+        [NotMapped]
+        public string CompIban { get; set; } = null!;
+
+        [Required]
+        [NotMapped]
+        public string HospitalIban { get; set; } = null!;
+
+        [Required]
+        [NotMapped]
+        [Range(0, double.MaxValue)]
+        public double Amount { get; set; }
 
         public virtual InsuranceComp Comp { get; set; } = null!;
         public virtual Hospital HospitalNameNavigation { get; set; } = null!;
