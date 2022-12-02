0a35a05846155b7674fb7dd280813daa9653df4c
Pavol Petrovič <palko.petrovic@gmail.com>
2022-12-02 21:39:54 +0100

    Peaople Controller - Update

--- EHealthCard/EHealthCard/Controllers/PeopleController.cs
+++ EHealthCard/EHealthCard/Controllers/PeopleController.cs
@@ -22,9 +22,46 @@ namespace EHealthCard.Controllers
 
         // GET: People
         public async Task<IActionResult> Index()
-        {                        
+        {
             var modelContext = _context.People.Include(p => p.ZipNavigation);
             return View(await modelContext.ToListAsync());
+            //return View(new List<Payment>());
+        }
+
+        public async Task<IActionResult> Search()
+        {
+            return View();
+        }
+
+        public async Task<IActionResult> SearchItems([Bind("PersonId,Zip")] Person person)
+        {
+            TempData["Message"] = "Corresponding Data Listed";
+            if (String.IsNullOrEmpty(person.PersonId) && String.IsNullOrEmpty(person.Zip))
+            {
+                return View("Index", new List<InsuranceComp>());
+            }
+
+            if (String.IsNullOrEmpty(person.PersonId))
+            {
+                return View("Index", await _context.People
+                          .Include(p => p.ZipNavigation)
+                          .Where(i => i.Zip == person.Zip)
+                          .ToListAsync());
+            }
+
+            if (String.IsNullOrEmpty(person.Zip))
+            {
+                return View("Index", await _context.People
+                          .Include(p => p.ZipNavigation)
+                          .Where(i => i.PersonId == person.PersonId)
+                          .ToListAsync());
+            }
+
+            return View("Index", await _context.People
+                          .Include(p => p.ZipNavigation)
+                          .Where(i => i.Zip == person.Zip)
+                          .Where(i => i.PersonId == person.PersonId)
+                          .ToListAsync());
         }
 
         // GET: People/Details/5
@@ -38,7 +75,7 @@ namespace EHealthCard.Controllers
             var person = await _context.People
                 .Include(p => p.ZipNavigation)
                 .FirstOrDefaultAsync(m => m.PersonId == id);
-            
+
             if (person == null)
             {
                 return NotFound();
@@ -72,7 +109,6 @@ namespace EHealthCard.Controllers
         // GET: People/Create
         public IActionResult Create()
         {
-            ViewData["Zip"] = new SelectList(_context.Cities, "Zip", "Zip");
             return View();
         }
 
@@ -81,18 +117,44 @@ namespace EHealthCard.Controllers
         // For more details, see http://go.microsoft.com/fwlink/?LinkId=317598.
         [HttpPost]
         [ValidateAntiForgeryToken]
-        public async Task<IActionResult> Create([Bind("PersonId,Zip")] Person person)
+        public async Task<IActionResult> Create([Bind("PersonId,Zip," + "FirstName,LastName,Phone,Email")] Person person)
         {
-            if (ModelState.IsValid)
+            OracleConnection connection = new OracleConnection("User Id=c##local;Password=oracle;Data Source=25.48.253.17:1521/xe;");
+            connection.Open();
+            OracleTransaction transaction;
+            transaction = connection.BeginTransaction(IsolationLevel.ReadCommitted);
+
+            OracleCommand command = new OracleCommand();
+            try
             {
-                _context.Add(person);
-                await _context.SaveChangesAsync();
+                command.Transaction = transaction;
+                command.Connection = connection;
+                command.CommandText = "INSERT INTO person values(:ID, :ZIP,person_inf(:First_Name,:Last_Name,:Phone,:Email))";
+                OracleParameter[] parameters = new OracleParameter[]
+                {
+                    new OracleParameter("ID", person.PersonId),
+                    new OracleParameter("ZIP", person.Zip),
+                    new OracleParameter("First_Name", person.FirstName),
+                    new OracleParameter("Last_Name", person.LastName),
+                    new OracleParameter("Phone", person.Phone),
+                    new OracleParameter("Email", person.Email)
+                };
+                command.Parameters.AddRange(parameters);
+
+                command.ExecuteNonQuery();
+                //Comit
+                transaction.Commit();
+                connection.Close();
                 return RedirectToAction(nameof(Index));
             }
-            ViewData["Zip"] = new SelectList(_context.Cities, "Zip", "Zip", person.Zip);
-            return View(person);
+            catch (Exception ex)
+            {
+                transaction.Rollback();
+                connection.Close();
+                TempData["Message"] = "Data Creation Failed";
+                return View(person);
+            }
         }
-
         // GET: People/Edit/5
         public async Task<IActionResult> Edit(string id)
         {
@@ -106,7 +168,6 @@ namespace EHealthCard.Controllers
             {
                 return NotFound();
             }
-            ViewData["Zip"] = new SelectList(_context.Cities, "Zip", "Zip", person.Zip);
             return View(person);
         }
 
@@ -115,7 +176,7 @@ namespace EHealthCard.Controllers
         // For more details, see http://go.microsoft.com/fwlink/?LinkId=317598.
         [HttpPost]
         [ValidateAntiForgeryToken]
-        public async Task<IActionResult> Edit(string id, [Bind("PersonId,Zip")] Person person)
+        public async Task<IActionResult> Edit(string id, [Bind("PersonId,Zip," + "FirstName,LastName,Phone,Email")] Person person)
         {
             if (id != person.PersonId)
             {
@@ -128,35 +189,30 @@ namespace EHealthCard.Controllers
                 {
                     _context.Update(person);
                     await _context.SaveChangesAsync();
+                    TempData["Message"] = "Data Edited";
+                    return RedirectToAction(nameof(Index));
                 }
                 catch (DbUpdateConcurrencyException)
                 {
-                    if (!PersonExists(person.PersonId))
-                    {
-                        return NotFound();
-                    }
-                    else
-                    {
-                        throw;
-                    }
+                    TempData["Message"] = "Data Edition Failed";
+                    return View(person);
                 }
-                return RedirectToAction(nameof(Index));
             }
-            ViewData["Zip"] = new SelectList(_context.Cities, "Zip", "Zip", person.Zip);
+            TempData["Message"] = "Data are not valid";
             return View(person);
         }
 
         // GET: People/Delete/5
-        public async Task<IActionResult> Delete(string id)
+        public async Task<IActionResult> Delete(Person p_person)
         {
-            if (id == null || _context.People == null)
+            if (p_person == null || _context.People == null)
             {
                 return NotFound();
             }
 
             var person = await _context.People
                 .Include(p => p.ZipNavigation)
-                .FirstOrDefaultAsync(m => m.PersonId == id);
+                .FirstOrDefaultAsync(m => m.PersonId == p_person.PersonId);
             if (person == null)
             {
                 return NotFound();
@@ -168,25 +224,24 @@ namespace EHealthCard.Controllers
         // POST: People/Delete/5
         [HttpPost, ActionName("Delete")]
         [ValidateAntiForgeryToken]
-        public async Task<IActionResult> DeleteConfirmed(string id)
+        public async Task<IActionResult> DeleteConfirmed([Bind("PersonId,Zip," + "FirstName,LastName,Phone,Email")] Person person)
         {
             if (_context.People == null)
             {
                 return Problem("Entity set 'ModelContext.People'  is null.");
             }
-            var person = await _context.People.FindAsync(id);
             if (person != null)
             {
+                TempData["Message"] = "Data Deleted";
                 _context.People.Remove(person);
             }
-            
+            else
+            {
+                TempData["Message"] = "Data Deletion Failed";
+            }
+
             await _context.SaveChangesAsync();
             return RedirectToAction(nameof(Index));
-        }
-
-        private bool PersonExists(string id)
-        {
-          return _context.People.Any(e => e.PersonId == id);
         }
     }
 }
