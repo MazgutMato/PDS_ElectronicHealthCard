f6fa257b36e34a019c611b75b4f4afc6a4184891
Matej Mažgút <mazgut13@stud.uniza.sk>
2022-12-03 17:12:39 +0100

    Namspace

--- EHealthCard/EHealthCard/Models/ModelContext.cs
+++ EHealthCard/EHealthCard/Models/ModelContext.cs
@@ -3,7 +3,7 @@ using System.Collections.Generic;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Metadata;
 
-namespace EHealthCard.ModelsNew
+namespace EHealthCard.Models
 {
     public partial class ModelContext : DbContext
     {
