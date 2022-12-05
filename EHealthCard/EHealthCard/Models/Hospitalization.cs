c4c2252b521f2e36712b4e5e9fda95d5213d5d33
Pavol Petrovič <palko.petrovic@gmail.com>
2022-12-05 11:12:33 +0100

    All View Design Update

--- EHealthCard/EHealthCard/Models/Hospitalization.cs
+++ EHealthCard/EHealthCard/Models/Hospitalization.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
 
 namespace EHealthCard.Models
 {
@@ -10,8 +11,15 @@ namespace EHealthCard.Models
             Diagnoses = new HashSet<Diagnosis>();
         }
 
+        [Required]
+        [StringLength(10, ErrorMessage = "Person ID has to be 10 chars long", MinimumLength = 10)]
         public string PersonId { get; set; } = null!;
+
+        [Required]
+        [StringLength(20)]
         public string HospitalName { get; set; } = null!;
+
+        [Required]
         public DateTime DateStart { get; set; }
         public DateTime? DateEnd { get; set; }
 
