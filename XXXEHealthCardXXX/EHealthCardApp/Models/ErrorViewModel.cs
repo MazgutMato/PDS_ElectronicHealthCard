6a19c9dfc039d9733177fee5b581d8811ce28a28
Pavol Petrovič <palko.petrovic@gmail.com>
2022-12-12 12:10:23 +0100

    XXX Delete

--- XXXEHealthCardXXX/EHealthCardApp/Models/ErrorViewModel.cs
+++
@@ -1,9 +0,0 @@
-namespace EHealthCardApp.Models
-{
-    public class ErrorViewModel
-    {
-        public string? RequestId { get; set; }
-
-        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
-    }
-}
